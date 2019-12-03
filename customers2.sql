685d702b021509579af073479debc13a3e94d6eb
lisethdayan0602 <liseth.cabrera-hansa@eastern.edu>
2019-12-03 00:07:34 -0800

    'addedlocation_id'

---
+++ customers2.sql
@@ -0,0 +1,21 @@
+CREATE TABLE customers(
+ company_id INTEGER,
+ company_name VARCHAR,
+ contact_name VARCHAR,
+ phone_number VARCHAR,
+ location_id VARCHAR
+ 
+);
+ 
+INSERT INTO customers
+VALUES ('1001', 'Pfizer', 'Patrick Miller', '267-490-6811', '202'),
+('1002', 'Johnson&Johnson', 'Marion Chandor', '456-867-4258', '203'),
+('1003', 'Novartis', 'Patricia Marin', '652-452-1479', '204'),
+('1004', 'Merck&Co', 'Laura Scott', '526-852-6312', '205'),
+('1005', 'Roche', 'Patric Henz', '852-456-1236', '210'),
+('1006', 'Eli Lilly', 'Maria Raz', '745-896-2545', '220'),
+('1007', 'Sanofi', 'Valeria Lugo', '512-896-4523', '221'),
+('1008', 'Amgen', 'Sofia Marc', '356-258-9514', '211'),
+('1009', 'Bayer', 'Paul Forbes', '159-852-3698', '206'),
+('1010', 'Allergan', 'Francy Mandis', '986-843-9632', '207');
+
